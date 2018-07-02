ee36b87e4bdf497f9a78bf0cb256ac54abb4c8fc
ajweeks <ajweeks@shaw.ca>
2018-07-02 09:34:09 +0100

    Fix projectile-fan interaction

--- NJ01/Assets/Scripts/Fan.cs
+++ NJ01/Assets/Scripts/Fan.cs
@@ -13,7 +13,7 @@ public class Fan : MonoBehaviour
     {
         public GameObject obj;
         public Vector3 vel;
-        public float startingY;
+        public float y;
     }
 
     private List<ProjectileRef> _projectilesInAirStream;
@@ -21,7 +21,7 @@ public class Fan : MonoBehaviour
 
     private float _minStickSpeed = 0.1f;
     private float _maxStickSpeed = 1.0f;
-    private float _fallMultiplier = 5.0f;
+    private float _fallMultiplier = -60.0f;
 
     void Start ()
 	{
@@ -29,7 +29,6 @@ public class Fan : MonoBehaviour
 
         _airFlowCapsule = GetComponentInChildren<CapsuleCollider>();
         int capsuleDir = _airFlowCapsule.direction;
-        Debug.Log(capsuleDir);
         if (capsuleDir == 0)
         {
             _airFlowDir = Vector3.right;
@@ -48,29 +47,30 @@ public class Fan : MonoBehaviour
     {
         float stickSpeed = Mathf.Abs(ControllingValve.GetAverageStickSpeed());
         float stickSpeedMult = Mathf.Clamp01(Mathf.Max(stickSpeed - _minStickSpeed, 0.0f) / (_maxStickSpeed - _minStickSpeed));
-
-        // Look at this fancy syntax ;)
-        _projectilesInAirStream.ForEach(projectile =>
-        {
-            projectile.vel = Vector3.Lerp(projectile.vel,
-                new Vector3(
+        
+            _projectilesInAirStream.ForEach(projectile =>
+            {
+                projectile.vel = new Vector3(
                     _airFlowDir.x * AirFlowSpeed * stickSpeedMult,
-                    0, //_fallMultiplier * stickSpeedMult,
-                    _airFlowDir.z * AirFlowSpeed * stickSpeedMult),
-                1.0f);
+                    0,
+                    _airFlowDir.z * AirFlowSpeed * stickSpeedMult);
 
-            projectile.obj.transform.position = new Vector3(
-                projectile.obj.transform.position.x + projectile.vel.x * Time.deltaTime,
-                stickSpeed > 0 ? projectile.startingY : projectile.obj.transform.position.y,
-                projectile.obj.transform.position.z + projectile.vel.z * Time.deltaTime);
-
-            //projectile.vel = Vector3.Lerp(projectile.vel, Vector3.zero, 0.1f);
+                if (stickSpeedMult < 0.1f)
+                {
+                    projectile.y += _fallMultiplier * Time.deltaTime;
+                }
+            
+                projectile.obj.transform.position = new Vector3(
+                    projectile.obj.transform.position.x + projectile.vel.x * Time.deltaTime,
+                    projectile.y,
+                    projectile.obj.transform.position.z + projectile.vel.z * Time.deltaTime);
         });
 
         for (int i = _projectilesInAirStream.Count - 1; i >= 0; i--)
         {
             if (_projectilesInAirStream[i].obj.GetComponent<Projectile>().HitTrigger)
             {
+                _projectilesInAirStream[i].obj.GetComponent<Rigidbody>().useGravity = true;
                 _projectilesInAirStream.RemoveAt(i);
             }
         }
@@ -84,12 +84,21 @@ public class Fan : MonoBehaviour
 
             if (!proj.HitTrigger)
             {
+                foreach (var projectile in _projectilesInAirStream)
+                {
+                    if (projectile.obj == other.gameObject)
+                    {
+                        // This project has already been added
+                        return;
+                    }
+                }
+
                 ProjectileRef newRef;
                 newRef.obj = other.gameObject;
                 newRef.obj.GetComponent<Rigidbody>().useGravity = false;
-                newRef.startingY = other.gameObject.transform.position.y;
-                newRef.vel = Vector3.zero;// other.GetComponent<Rigidbody>().velocity;
-                //other.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                newRef.y = other.gameObject.transform.position.y;
+                newRef.vel = other.GetComponent<Rigidbody>().velocity;
+                other.GetComponent<Rigidbody>().velocity = Vector3.zero;
                 _projectilesInAirStream.Add(newRef);
             }
         }
